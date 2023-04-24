3b97eedec9ce92507fd2a345f52b83d8f70805a0
下津浦勝 <shimotsuurasho@shitatsuuramasarunoMacBook-Air.local>
2023-04-24 16:36:37 +0900

    seed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,18 @@
 end
 
 5.times do |n|
-  School.create!(
+  school = School.new(
     name: "name#{n + 1}",
     prefecture: "prefecture#{n + 1}",
     city: "city#{n + 1}",
     phone: "phone#{n + 1}",
     overview: "overview#{n + 1}",
     user_id: n + 1
-    image_path = Rails.root.join("db/images/image#{n + 1}.jpg")
-    school.image.attach(io: File.open(image_path), filename: "image#{n + 1}.jpg", content_type: "image/jpeg")
   )
+  image_path = Rails.root.join("db/images/image#{n + 1}.jpg")
+  school.image.attach(io: File.open(image_path), filename: "image#{n + 1}.jpg", content_type: "image/jpeg")
+  school.save!
 end
-
-
 
 5.times do |n|
   Subject.create!(
