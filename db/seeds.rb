1cd3e7d3f7a4f96822d90247a60b15b446f48c12
下津浦勝 <shimotsuurasho@shitatsuuramasarunoMacBook-Air.local>
2023-04-24 16:25:09 +0900

    seedの変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-10.times do |n|
+5.times do |n|
   User.create!(
     email: "test#{n + 1}@gmail.com",
     name: "テスト#{n + 1}",
@@ -7,7 +7,7 @@
   )
 end
 
-10.times do |n|
+5.times do |n|
   School.create!(
     name: "name#{n + 1}",
     prefecture: "prefecture#{n + 1}",
@@ -15,16 +15,20 @@ end
     phone: "phone#{n + 1}",
     overview: "overview#{n + 1}",
     user_id: n + 1
+    image_path = Rails.root.join("db/images/image#{n + 1}.jpg")
+    school.image.attach(io: File.open(image_path), filename: "image#{n + 1}.jpg", content_type: "image/jpeg")
   )
 end
 
-10.times do |n|
+
+
+5.times do |n|
   Subject.create!(
     name: "科目#{n + 1}"
   )
 end
 
-10.times do |n|
+5.times do |n|
   Handling.create!(
     school_id: n + 1,
     subject_id: n + 1
